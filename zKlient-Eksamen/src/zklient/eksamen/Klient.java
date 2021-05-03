ba407f21efbb89a0a584c80036cc3362bbb5bd4e
🎃 Christoffer 🎃 <strand.arnesen@gmail.com>
2021-05-03 12:47:46 +0200

    initialiaisering

---
+++ zKlient-Eksamen/src/zklient/eksamen/Klient.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zklient.eksamen;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Klient extends Application {
+    static BorderPane bpane;
+    //Login deklarasjoner
+    static Pane topLogin = new Pane();
+    static Pane centerLogin = new Pane();
+    static Pane bottomLogin = new Pane();
+    static Label topLabelLogin = new Label("Tast inn brukernavn: ");
+    static TextField txtLogin = new TextField();
+    static Button btnLogin = new Button("Log inn");
+    
+    //Rom deklarasjoner
+    static Pane topRom = new Pane();
+    static Pane centerRom = new Pane();
+    static Label labelRom = new Label("Chat-Rom: ");
+    static ListView list = new ListView();
+    
+    
+    
+    @Override
+    public void start(Stage primaryStage) {
+    
+        bpane = new BorderPane();
+        /* Login side panes */
+        bpane.setTop(topLogin);
+        bpane.setCenter(centerLogin);
+        bpane.setBottom(bottomLogin);
+        
+        topLogin.setPrefHeight(100);
+        topLogin.setStyle("-fx-border-color: black; -fx-background-color: grey;");
+        centerLogin.setPrefHeight(300);
+        centerLogin.setStyle("-fx-border-color: black; -fx-background-color: white;");
+        bottomLogin.setPrefHeight(100);
+        bottomLogin.setStyle("-fx-border-color: black; -fx-background-color: grey;");
+        topLabelLogin.setStyle("-fx-text-fill:BLACK; -fx-font-size: 30;");
+        
+        topLogin.getChildren().add(topLabelLogin);
+        topLabelLogin.setLayoutX(25);
+        topLabelLogin.setLayoutY(25);
+        
+        centerLogin.getChildren().add(txtLogin);
+        txtLogin.setLayoutX(150);
+        txtLogin.setLayoutY(140);
+        centerLogin.getChildren().add(btnLogin);
+        btnLogin.setLayoutX(350);
+        btnLogin.setLayoutY(140);
+        /* Slutt på Login panes */
+        
+        /*  Rom side panes */
+        topRom.setPrefHeight(50);
+        topRom.setStyle("-fx-border-color: black; -fx-background-color: grey;");
+        centerRom.setPrefHeight(450);
+        centerRom.setStyle("-fx-border-color: black; -fx-background-color: white;");
+        labelRom.setStyle("-fx-text-fill:BLACK; -fx-font-size: 30;");
+        
+        labelRom.setLayoutX(25);
+        labelRom.setLayoutY(7);
+        
+        
+        Scene scene = new Scene(bpane, 600, 500);
+        
+        primaryStage.setTitle("Klient");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    public static void logInn() {
+    btnLogin.setOnAction((event) -> {
+        bpane.getChildren().remove(centerLogin);
+        bpane.getChildren().remove(topLogin);
+        bpane.getChildren().remove(bottomLogin);
+        
+        bpane.setTop(topRom);
+        bpane.setCenter(centerRom);
+        topRom.getChildren().add(labelRom);
+    
+    });
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        logInn();
+        launch(args);
+    }
+    
+}
---
+++ zTjener-Eksamen/src/ztjener/eksamen/Tjener.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ztjener.eksamen;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import java.sql.*;
+
+
+public class Tjener extends Application {
+    
+    private static String url = "jdbc:sqlite:eksamen.db"; 
+    //Socket
+    int port = 8000;
+    ObjectOutputStream out;
+    ObjectInputStream in;
+    ServerSocket server;
+    Socket socket;
+    
+    
+    private Connection connectDB() {
+        //String url = "jdbc:sqlite:C:\\Users\\Mats Engesund\\Documents\\NetBeansProjects\\OBJ2100\\eksamen.db";
+        Connection con = null; 
+        try {
+            con = DriverManager.getConnection(url); 
+            System.out.println("Tilkobling til DB er etablert");
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return con;
+    }
+    
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Tjener app = new Tjener(); 
+        app.connectDB();
+        
+        BorderPane bpane = new BorderPane();
+        //root.getChildren().add(btn);
+        
+        Scene scene = new Scene(bpane, 600, 500);
+        
+        primaryStage.setTitle("Tjener");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
