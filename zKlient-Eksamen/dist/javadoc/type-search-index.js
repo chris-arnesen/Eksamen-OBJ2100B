6ab84cd1783b1f56c63cf719882a511fc26e536b
Cobra Bubbles <mjoeng@hotmail.no>
2021-05-05 23:32:24 +0200

    Javadoc

---
+++ zKlient-Eksamen/dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"zklient.eksamen","l":"Klient"},{"p":"zklient.eksamen","l":"klientThread"}]
--- zKlient-Eksamen/src/zklient/eksamen/Klient.java
+++ zKlient-Eksamen/src/zklient/eksamen/Klient.java
@@ -5,16 +5,9 @@
  */
 package zklient.eksamen;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -34,9 +27,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
@@ -52,12 +43,14 @@ public class Klient extends Application {
         }
     }
     
+    
     enum Type {
         BNAVN, MELDING, ROM, JOIN, REMOVE
     }
     
     static String outputInfo ="";
     static Type type;
+    
     
     //Socket
     static int port = 8000;
@@ -106,8 +99,8 @@ public class Klient extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-    
         bpane = new BorderPane();
+        
         /* Login side panes */
         bpane.setTop(topLogin);
         bpane.setCenter(centerLogin);
@@ -139,10 +132,7 @@ public class Klient extends Application {
                 }
             } catch (IOException ex) {System.out.println("Feil med login-funksjon");}
         });
-        //Det som skal skje dersom en bruker trykker på knappen for å sende en melding
-        /*btnChat.setOnAction(e -> {
-            if (txt)
-        });*/
+        
         btnChat.setOnAction(e -> {
             try {
                 if (txtChat.getText().equals("")) {
@@ -195,6 +185,7 @@ public class Klient extends Application {
                         String utTekst = type.JOIN.name() + ";" + list.getSelectionModel().getSelectedItem() + ";" + bNavn;
                         aktivtRom = (String)list.getSelectionModel().getSelectedItem();
                         labelChat.setText(aktivtRom);
+                        
                         //Denne for-loopen viser alle meldinger tilhørende til det rommet, når en trykker seg inn på et rom
                         for (String s :alleMeldinger) {
                             String[] split = s.split(":"); //melding:romnr
@@ -226,11 +217,6 @@ public class Klient extends Application {
         btnNew.setLayoutY(7);
         btnNew.setOnAction(e -> openInput());
         
-        /*list.getItems().add("Item 1");
-        list.getItems().add("Item 2");
-        list.getItems().add("Item 3");*/
-        /* Slutt på rom side panes */
-        
         /* Chat side panes */
         topChat.setPrefHeight(50);
         topChat.setStyle("-fx-border-color: black; -fx-background-color: grey;");
@@ -257,6 +243,7 @@ public class Klient extends Application {
         topChat.getChildren().add(btnBack);
         /* Slutt på Chat side panes */
         
+        
         Scene scene = new Scene(bpane, 600, 500);
         primaryStage.setTitle("Klient");
         primaryStage.setScene(scene);
@@ -275,44 +262,6 @@ public class Klient extends Application {
         }catch(IOException ex){System.out.println("Tilkobling til server feilet");}
         
     }
-    
-    
-    
-    
-    /*public static void logInn() {
-        
-        btnLogin.setOnAction((event) -> {
-            
-            bNavn = txtLogin.getText();
-            if (bNavn.equals("")) {
-                System.out.println("Navn-feltet er tomt");
-            } else {
-                try {
-                    socket = new Socket(host, port);
-                    out = new ObjectOutputStream(socket.getOutputStream());
-                    
-                    String outputInfo = type.BNAVN.name() + ";";
-                    outputInfo+=bNavn;
-                    
-                    out.writeObject(outputInfo);
-                    
-                    out.close();
-                    socket.close();
-                    
-                    bpane.getChildren().remove(centerLogin);
-                    bpane.getChildren().remove(topLogin);
-                    bpane.getChildren().remove(bottomLogin);
-                    
-                    bpane.setTop(topRom);
-                    bpane.setCenter(centerRom);
-                    bpane.setBottom(bottomRom);
-                    topRom.getChildren().add(labelRom);
-                    centerRom.getChildren().add(list);
-                    bottomRom.getChildren().add(btnNew);
-                } catch (IOException ex) { System.out.println("Feil med forbindelse til tjener"); }
-            }
-        });   
-    }*/
     
     
     public void openInput() {
@@ -342,6 +291,7 @@ public class Klient extends Application {
         }
     }
     
+    
     public static void backBtn() {
         btnBack.setOnAction(event -> {  
             forlatRommet();
@@ -356,99 +306,15 @@ public class Klient extends Application {
         });
     }
     
-    /*
-    public void createNewRoom(String txt) {
-        try {
-            socket = new Socket(host, port);
-            out = new ObjectOutputStream(socket.getOutputStream()); 
-            
-            String outputInfo = type.ROM.name() + ";";
-            outputInfo += txt;        
-            out.writeObject(outputInfo);
-            
-                        bpane.getChildren().remove(topRom);
-                        bpane.getChildren().remove(centerRom);
-                        bpane.getChildren().remove(bottomRom);
-
-                        bpane.setTop(topChat);
-                        bpane.setCenter(centerChat);
-                        bpane.setBottom(bottomChat);
-                      
-            out.close();
-            socket.close();
-        } catch (IOException ex) {
-            Logger.getLogger(Klient.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }*/
-    
-    /*
-    public void addRoomToList(String txt) {
-        list.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent t) {
-                try {
-                    System.out.println("Du trykket på: " + list.getSelectionModel().getSelectedItem());
-                    socket = new Socket(host, port);
-                    out = new ObjectOutputStream(socket.getOutputStream());
-                    
-                    String outputInfo = type.JOIN.name() + ";";
-                    outputInfo += list.getSelectionModel().getSelectedItem();
-                    out.writeObject(outputInfo);
-                    
-                    /*
-                        Her kommer det kode på hva som skjer etter
-                        de har joinet/blitt lagt til i liste med rom:
-                    */
-                       
-                    /*
-                    in = new ObjectInputStream(socket.getInputStream());
-                    String klientInput = (String)(in.readObject()); 
-                    System.out.println(klientInput);
-                    
-                    socket.close();
-                    out.close();
-                } catch (IOException ex) {
-                    Logger.getLogger(Klient.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (ClassNotFoundException ex) {
-                    Logger.getLogger(Klient.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        });
-        list.getItems().add(txt);
-    }*/
-    
-    /*
-    public static void chat() {
-        btnNew.setOnAction((event) -> {
-        bpane.getChildren().remove(topRom);
-        bpane.getChildren().remove(centerRom);
-        bpane.getChildren().remove(bottomRom);
-        
-        
-        bpane.setTop(topChat);
-        bpane.setCenter(centerChat);
-        bpane.setBottom(bottomChat);
-        topChat.getChildren().add(labelChat);
-        bottomChat.getChildren().add(txtChat);
-        bottomChat.getChildren().add(btnChat);
-        
-    }); 
-    }
-    */
-
-    /*
-
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //sendMelding();
-        //logInn();
         backBtn();
-        //chat();
         launch(args);
     }
+    
     
     private class ButtonListener implements EventHandler<ActionEvent> {
         @Override
